ae87b455a2d696cc7a52bb380a1f8376de1538d5
famunozro <famunozro@gmail.com>
2026-02-08 21:37:06 -0500

    updates app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mi_proyecto
 proyecto herramientas de desarrollo de software
+
+Esta es una aplicacion web que graficas de datos de ventas vehiculos
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv(r'C:\Users\Fabian\OneDrive\TRIPLETEN\mi_proyecto\vehicles_us.csv') # leer los datos
+
+st.header('Proyecto de aplicacion webs obre vehiculos')
+hist_button = st.button('Construir histograma') # crear un botón
+
+ if hist_button: # al hacer clic en el botón
+         # escribir un mensaje
+         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+         fig = px.histogram(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+         st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir dispersion') # crear un botón
+     
+     if disp_button: # al hacer clic en el botón
+         # escribir un mensaje
+         st.write('Creación de una dispersion para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear una dispersion
+         fig = px.scatter(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+         st.plotly_chart(fig)
